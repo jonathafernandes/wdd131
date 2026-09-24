31020d62ed8523dfbb6f6ba60ae9da9796c25c67
Jonatha Fernandes <jonathafernandes@live.com>
2026-09-24 14:17:32 -0300

    ajustes da Auditoria da Página

--- scripts/local.js
+++ scripts/local.js
@@ -10,7 +10,7 @@ function calcularSensacaoTermica(temperatura, umidade) {
 
 if (temperatura <= 10 && velocidadeVento > 4.8) {
     calcularSensacaoTermica(temperatura, umidade)
-    sensacao.innerHTML = `<dt>Sensação térmica: ${sensacaoTermica}</dt>`;
+    sensacao.innerHTML = `<dd><dt>Sensação térmica: ${sensacaoTermica}</dt></dd>`;
 } else {
     sensacao.innerHTML = "<dt>Temperatura: N/A</dt>";
 }
